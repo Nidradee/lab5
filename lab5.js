4552f1f29dd63a412b56c995a4aaa5bb40fd0d5a
Nidradee <deesmity7@yahoo.com>
2016-02-05 11:53:31 -0500

    New File

---
+++ lab5.js
@@ -0,0 +1,11 @@
+var fah = 65
+var cel = 100
+
+var celsius = fah -32*(5/9);
+console.log(celsius + " Here is the equalation to convert Fahrenheit to Celsius");
+
+
+var fahrenheit = cel + 32*(9/5);
+console.log(fahrenheit + " Here is the equalation to convert Celsius to Fahrenheit ");
+
+
---
+++ lab5bonus.js
@@ -0,0 +1,14 @@
+/*var fah = 65
+var cel = 100
+
+var celsius = fah -32*(5/9);
+console.log(celsius + " Here is the equalation to convert Fahrenheit to Celsius");
+
+
+var fahrenheit = cel + 32*(9/5);
+console.log(fahrenheit + " Here is the equalation to convert Celsius to Fahrenheit ");
+*/
+
+var fahrenheit = prompt ("what is todays temperature");
+var celsius = fahrenheit - 32 * 5/9;
+document.write( "Here is todays fahrenheit temperture converted to Celsius is " + celsius);
